76d7f353040646fb2651d2331b1d0714950ec998
Ukjin <shddnrwls@gmail.com>
2017-08-17 09:26:56 +0000

    yeah

--- app/controllers/sizingreal_controller.rb
+++ app/controllers/sizingreal_controller.rb
@@ -7,11 +7,30 @@ class SizingrealController < ApplicationController
 
   def result
     @hash_jean_size = Hash.new
-    @hash_jean_size = {"length" => params[:length],
-    "waist" => params[:waist],
-    "thigh" => params[:thigh],
-    "rise" => params[:rise],
-    "tail" => params[:tail]}
+    @hash_jean_size = {
+    "length" => params[:result_length],
+    "waist" => params[:result_waist],
+    "thigh" => params[:result_thigh],
+    "rise" => params[:result_rise],
+    "tail" => params[:result_tail]}
+        
+    @hash_jean_size2 = Hash.new
+    @hash_jean_size2 = {
+    "length2" => params[:result_length2],
+    "waist2" => params[:result_waist2],
+    "thigh2" => params[:result_thigh2],
+    "rise2" => params[:result_rise2],
+    "tail2" => params[:result_tail2]}
+
+    @hash_first_size = Hash.new
+    @hash_first_size = {
+      "first_size" => params[:first_size]
+    }
+    
+    @hash_second_size = Hash.new
+    @hash_second_size = {
+      "second_size" => params[:second_size]
+    }
 
     @hash_my_size = Hash.new
     @hash_my_size = {"m_length" => params[:m_length],
@@ -25,6 +44,13 @@ class SizingrealController < ApplicationController
     @diff_thigh = (@hash_jean_size["thigh"]).to_f / (@hash_my_size["m_thigh"]).to_f
     @diff_rise = (@hash_jean_size["rise"]).to_f / (@hash_my_size["m_rise"]).to_f
     @diff_tail = (@hash_jean_size["tail"]).to_f / (@hash_my_size["m_tail"]).to_f
+
+  
+    @diff_length2 = (@hash_jean_size2["length2"]).to_f / (@hash_my_size["m_length"]).to_f
+    @diff_waist2 = (@hash_jean_size2["waist2"]).to_f / (@hash_my_size["m_waist"]).to_f
+    @diff_thigh2 = (@hash_jean_size2["thigh2"]).to_f / (@hash_my_size["m_thigh"]).to_f
+    @diff_rise2 = (@hash_jean_size["rise2"]).to_f / (@hash_my_size["m_rise"]).to_f
+    @diff_tail2 = (@hash_jean_size2["tail2"]).to_f / (@hash_my_size["m_tail"]).to_f
 
   end
 
