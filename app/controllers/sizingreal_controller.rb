06cf4a6409a10f97dea7d5064263be34337379dc
Ukjin <shddnrwls@gmail.com>
2017-08-18 13:07:42 +0000

    middle

--- app/controllers/sizingreal_controller.rb
+++ app/controllers/sizingreal_controller.rb
@@ -39,18 +39,66 @@ class SizingrealController < ApplicationController
     "m_rise" => params[:m_rise],
     "m_tail" => params[:m_tail]}
 
-    @diff_length = (@hash_jean_size["length"]).to_f / (@hash_my_size["m_length"]).to_f
-    @diff_waist = (@hash_jean_size["waist"]).to_f / (@hash_my_size["m_waist"]).to_f
-    @diff_thigh = (@hash_jean_size["thigh"]).to_f / (@hash_my_size["m_thigh"]).to_f
-    @diff_rise = (@hash_jean_size["rise"]).to_f / (@hash_my_size["m_rise"]).to_f
-    @diff_tail = (@hash_jean_size["tail"]).to_f / (@hash_my_size["m_tail"]).to_f
-
+    if (@hash_jean_size["length"]).to_f == 0
+      @diff_length = 1.0
+    else
+      @diff_length = (@hash_jean_size["length"]).to_f / (@hash_my_size["m_length"]).to_f
+    end
+    
+    if (@hash_jean_size["waist"]).to_f == 0
+      @diff_waist = 1.0
+    else
+      @diff_waist = (@hash_jean_size["waist"]).to_f / (@hash_my_size["m_waist"]).to_f
+    end
+    
+    if (@hash_jean_size["thigh"]).to_f == 0
+      @diff_thigh = 1.0
+    else
+      @diff_thigh = (@hash_jean_size["thigh"]).to_f / (@hash_my_size["m_thigh"]).to_f
+    end
+    
+    if (@hash_jean_size["rise"]).to_f == 0
+      @diff_rise = 1.0
+    else
+      @diff_rise = (@hash_jean_size["rise"]).to_f / (@hash_my_size["m_rise"]).to_f
+    end
+    
+    if (@hash_jean_size["tail"]).to_f == 0
+      @diff_tail = 1.0
+    else
+      @diff_tail = (@hash_jean_size["tail"]).to_f / (@hash_my_size["m_tail"]).to_f
+    end
   
-    @diff_length2 = (@hash_jean_size2["length2"]).to_f / (@hash_my_size["m_length"]).to_f
-    @diff_waist2 = (@hash_jean_size2["waist2"]).to_f / (@hash_my_size["m_waist"]).to_f
-    @diff_thigh2 = (@hash_jean_size2["thigh2"]).to_f / (@hash_my_size["m_thigh"]).to_f
-    @diff_rise2 = (@hash_jean_size["rise2"]).to_f / (@hash_my_size["m_rise"]).to_f
-    @diff_tail2 = (@hash_jean_size2["tail2"]).to_f / (@hash_my_size["m_tail"]).to_f
+  
+    if (@hash_jean_size2["length2"]).to_f == 0
+      @diff_length2 = 1.0
+    else
+      @diff_length2 = (@hash_jean_size2["length2"]).to_f / (@hash_my_size["m_length"]).to_f
+    end
+    
+    if (@hash_jean_size2["waist2"]).to_f == 0
+      @diff_waist2 = 1.0
+    else
+      @diff_waist2 = (@hash_jean_size2["waist2"]).to_f / (@hash_my_size["m_waist"]).to_f
+    end
+    
+    if (@hash_jean_size2["thigh2"]).to_f == 0
+      @diff_thigh2 = 1.0
+    else
+      @diff_thigh2 = (@hash_jean_size2["thigh2"]).to_f / (@hash_my_size["m_thigh"]).to_f
+    end
+    
+    if (@hash_jean_size2["rise2"]).to_f == 0
+      @diff_rise2 = 1.0
+    else
+      @diff_rise2 = (@hash_jean_size2["rise2"]).to_f / (@hash_my_size["m_rise"]).to_f
+    end
+    
+    if (@hash_jean_size2["tail2"]).to_f == 0
+      @diff_tail2 = 1.0
+    else
+      @diff_tail2 = (@hash_jean_size2["tail2"]).to_f / (@hash_my_size["m_tail"]).to_f
+    end
 
     @minus_waist1 = ((@hash_jean_size["waist"]).to_f - (@hash_my_size["m_waist"]).to_f) * 3.0
     @minus_waist2 = ((@hash_jean_size2["waist2"]).to_f - (@hash_my_size["m_waist"]).to_f) * 3.0
--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,14 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SizingReal
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
